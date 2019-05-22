a9b6135e1930b5abeef3fb5d42220c76c660cce9
Saikiran <Saikiran@DESKTOP-2FHRIEM>
2019-05-22 11:38:43 +0530

    Completed!

--- src/main/java/main/SampleProject.java
+++ src/main/java/main/SampleProject.java
@@ -1,14 +1,15 @@
 package main;
 
 public class SampleProject {
-	//print  Hello world! in the console
+	
 	public static String printHello() {
-		//print Hello world! in the console!
-		String c = "Hello rvabddld!";
+		
+		String c = "";
+		c = "Hello World!";
 		return c;
 	}
 	public static int add(int a,int b) {
-		int c = a+b;
+		int c = a + b;
 		return c;
 	}
 	public static void main(String args[]) {
